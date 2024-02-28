b4ebe7590d7f1d0fc14365644e176c6f5e63704f
FrancoMastro07 <francomastro07@gmail.com>
2024-02-28 19:36:07 -0300

    Added final boss

--- realm_royale/Character_properties.java
+++ realm_royale/Character_properties.java
@@ -1,8 +1,5 @@
 package realm_royale;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 import javax.swing.*;
 
 public class Character_properties {
@@ -1357,10 +1354,10 @@ public class Character_properties {
                     if(character_mana>=120){
 
                         character_mana -= 120;
-                        enemy_health -= 1800;
+                        enemy_health -= 2200;
                         character.scores(240);
-                        total_damage += 1800;
-                        character_action = "You made " + 1800 + " of damage with nuke\nYour current mana: " + character_mana + ". Enemy health: " + enemy_health;
+                        total_damage += 2200;
+                        character_action = "You made " + 2200 + " of damage with nuke\nYour current mana: " + character_mana + ". Enemy health: " + enemy_health;
 
                     }else{
 
@@ -1404,8 +1401,6 @@ public class Character_properties {
         character_level++;
         character.levels(character_level);
 
-        int last_health = character.health_character();
-        int last_mana = character.mana();
         character_health = character.health_character();
         character_protection = character.protection_character();
         character_mana = character.mana();
@@ -1415,6 +1410,274 @@ public class Character_properties {
         System.out.println("Health: " + character_health + "\nProtection: " + character_protection + "\nMana: " + character_mana + "\nDamage: " + character_damage);
 
         character_health += character_protection;
+
+        JOptionPane.showMessageDialog(null, "Something is weird...", "...", 2);
+        System.out.println(".........................................................................................................");
+
+        enemy.change_name_enemy("Cthulhu");
+        enemy.change_damage_enemy(1400);
+        enemy.change_health_enemy(6000);
+        enemy.change_protection_enemy(1500);
+
+        enemy_name = enemy.name_enemy();
+        enemy_health = enemy.health_enemy() + enemy.protection_enemy();
+        enemy_damage = enemy.damage_enemy();
+        enemy_ultradamage = enemy.damage_enemy() + 600;
+
+        JOptionPane.showMessageDialog(null, enemy_name + " has appeared", "Boss appeared", 2);
+        System.out.println(enemy_name + " Has " + enemy_health + " of health plus protection and make " + enemy_damage + " of damage");
+
+        character_action="";
+        enemy_action="";
+        total_damage=0;
+        round=1;
+
+//----------------------------------------------------------last-fight------------------------------------------------------------------------------------------------------------------------------
+
+        while(character_health>0 && enemy_health>0){        
+
+            byte random_attack = enemy.random_attack();
+            
+            System.out.println(".........................................................................................................");
+            System.out.println("Round " + round + "!\n");
+        
+            //-----------------------------enemy-action-------------------------------------------------------------------------------------------------------------------------------------
+
+            if(enemy_health<=(enemy_health/2)){
+
+                if(random_attack==1){
+
+                    character_health -= enemy_damage;
+                    enemy_action = enemy_name + " made " + enemy_damage + " of damage\nYour current mana: " + character_mana + ". Your current health: " + character_health;
+    
+                }else if(random_attack==2){
+
+                    enemy_health += 800;
+                    enemy_action = enemy_name + " healed itself, its current health is " + enemy_health; 
+
+                }else{
+
+                    character_health -= enemy_ultradamage;
+                    enemy_action = enemy_name + " made " + enemy_ultradamage + " of ultradamage\nYour current mana: " + character_mana + ". Your current health: " + character_health;
+
+                }
+
+            }else{
+
+                if(random_attack==1){
+
+                    character_health -= enemy_damage;
+                    enemy_action = enemy_name + " made " + enemy_damage + " of damage\nYour current mana: " + character_mana + ". Your current health: " + character_health;
+    
+                }else if(random_attack==2){
+
+                    character_health -= enemy_ultradamage;
+                    enemy_action = enemy_name + " made " + enemy_ultradamage + " of ultradamage\nYour current mana: " + character_mana + ". Your current health: " + character_health;
+
+                }else{
+                    
+                    enemy_action = enemy_name + " has failed";
+
+                }
+
+            }
+            
+            if(character_health>0){
+
+                System.out.println(enemy_action);
+
+            }else{
+
+                Window window = new Window(character.score, 2);
+                window.setVisible(true);
+                window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+                JOptionPane.showMessageDialog(null, "You were defeated!!!", "WTF!!!", 2);
+
+                System.out.println("You were defeated");
+                System.out.println(".........................................................................................................");
+                System.exit(0);
+
+            }
+
+            System.out.println("------------------------");
+
+            //--------------------------------character-action-----------------------------------------------------------------------------------------------------------------------------------------
+
+            do{
+
+                action = JOptionPane.showInputDialog("What do you want to do?\n.Attack - A\n.Special attack - S\n.Objects - O\n.Pass - P");
+
+            }while(action.equalsIgnoreCase("a")==false && action.equalsIgnoreCase("o")==false && action.equalsIgnoreCase("s")==false && action.equalsIgnoreCase("p")==false);
+
+            if(action.equalsIgnoreCase("a")){                                                //attack
+
+                enemy_health -= character_damage;
+                character_action = "You made " + character_damage + " of damage\nEnemy health: " + enemy_health;
+                character.scores(50);
+                total_damage += character_damage;
+
+            }else if(action.equalsIgnoreCase("o")){                                //potions
+
+                do{
+
+                    object_action = JOptionPane.showInputDialog("Choose one object:\nHealth potion - HP\nMana potion - MP");
+
+                }while(object_action.equalsIgnoreCase("hp")==false && object_action.equalsIgnoreCase("mp")==false);
+
+                if(object_action.equalsIgnoreCase("hp")){                   //health potion
+
+                    if(inventory[0]>0){
+
+                        inventory[0] -= 1;
+                        character_health += 800;
+                        if(character_health>=(first_health + character_protection)){
+
+                            character_health = (first_health + character_protection);
+
+                        }
+                        character_action = "You healed yourself\nYour current mana: " + character_mana + ". Your current health: " + character_health + ". Health potions left: " + inventory[0];
+
+                    }else{
+
+                        character_action = "You cannot heal you";
+
+                    }
+
+                }else if(object_action.equalsIgnoreCase("mp")){             //mana potion
+
+                    if(inventory[1]>0){
+
+                        inventory[1] -= 1;
+                        character_mana += 70;
+                        if(character_mana>=first_mana){
+
+                            character_mana = first_mana;
+
+                        }
+                        character_action = "You got more mana\nYour current mana: " + character_mana + ". Your current health: " + character_health + ". Mana potions left: " + inventory[1];
+
+                    }else{
+
+                        character_action = "You cannot get more mana";
+
+                    }
+
+                }
+
+            }else if(action.equalsIgnoreCase("s")){                         //special attack      
+
+                do{
+
+                    special_action = JOptionPane.showInputDialog("Choose one special attack:\n.Fire - F (40 mana)\n.Ice - I (30 mana)\n.Light - L (60 mana)\n.Dark - D (90 mana)\n.Nuke - N (120 mana)");
+    
+                }while(special_action.equalsIgnoreCase("f")==false && special_action.equalsIgnoreCase("i")==false && special_action.equalsIgnoreCase("l")==false && special_action.equalsIgnoreCase("d")==false && special_action.equalsIgnoreCase("n")==false); 
+
+                if(special_action.equalsIgnoreCase("f")){
+
+                    if(character_mana>=40){
+
+                        character_mana -= 40;
+                        enemy_health -= 1200;
+                        character.scores(120);
+                        total_damage += 1200;
+                        character_action = "You made " + 1200 + " of damage with fire\nYour current mana: " + character_mana + ". Enemy health: " + enemy_health;
+
+                    }else{
+
+                        character_action = "You dont have enough mana to use this special attack";
+
+                    }
+                    
+                }else if(special_action.equalsIgnoreCase("i")){
+
+                    if(character_mana>=30){
+
+                        character_mana -= 30;
+                        enemy_health -= 1150;
+                        character.scores(100);
+                        total_damage += 1150;
+                        character_action = "You made " + 1150 + " of damage with ice\nYour current mana: " + character_mana + ". Enemy health: " + enemy_health;
+
+                    }else{
+
+                        character_action = "You dont have enough mana to use this special attack";
+
+                    }
+
+                }else if(special_action.equalsIgnoreCase("l")){
+
+                    if(character_mana>=60){
+
+                        character_mana -= 60;
+                        enemy_health -= 1500;
+                        character.scores(160);
+                        total_damage += 1500;
+                        character_action = "You made " + 1500 + " of damage with light\nYour current mana: " + character_mana + ". Enemy health: " + enemy_health;
+
+                    }else{
+
+                        character_action = "You dont have enough mana to use this special attack";
+
+                    }
+
+                }else if(special_action.equalsIgnoreCase("d")){
+
+                    if(character_mana>=90){
+
+                        character_mana -= 90;
+                        enemy_health -= 1900;
+                        character.scores(210);
+                        total_damage += 1900;
+                        character_action = "You made " + 1900 + " of damage with dark\nYour current mana: " + character_mana + ". Enemy health: " + enemy_health;
+
+                    }else{
+
+                        character_action = "You dont have enough mana to use this special attack";
+
+                    }
+
+                }else if(special_action.equalsIgnoreCase("n")){
+
+                    if(character_mana>=120){
+
+                        character_mana -= 120;
+                        enemy_health -= 2500;
+                        character.scores(240);
+                        total_damage += 2500;
+                        character_action = "You made " + 2500 + " of damage with nuke\nYour current mana: " + character_mana + ". Enemy health: " + enemy_health;
+
+                    }else{
+
+                        character_action = "You dont have enough mana to use this special attack";
+
+                    }
+
+                }
+
+            }else if(action.equalsIgnoreCase("p")){
+
+                character_action = "You ignored the enemy";
+
+            }
+
+            round++;
+
+            if(enemy_health>0){
+
+                System.out.println(character_action);
+
+            }else{
+
+                System.out.println(enemy_name + " was defeated");
+
+            }
+
+        }
+
+        System.out.println(".........................................................................................................");
+        System.out.println("Congratulations, you defeated " + enemy_name + " and finished the game." + "\nYou made " + total_damage + " of damage and your score is " + character.score + " points");
+        System.out.println(".........................................................................................................");
 
         Window window = new Window(character_health, character_mana, character.score, total_damage, 1);
         window.setVisible(true);
